1dbf7c000756f3992d3ff98dc9ed53398e486a36
Wei Luo <ryan.luo89@gmail.com>
2016-02-13 00:08:00 -0800

    217 Contains Duplicate

---
+++ 217_cd/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> numsSet = new HashSet<>();
+        for (int n : nums) {
+            if(numsSet.contains(n)) {
+                return true;
+            }
+            numsSet.add(n);
+        }
+        return false;
+    }
+}
